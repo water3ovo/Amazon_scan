181c6400ca3430f4aefcd01bbfa50b3d220cadab
water3ovo <1367804732@qq.com>
2026-09-04 16:54:04 +0800

    Bump parser hotfix version to beta5.3

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 """Amazon AE/SA scan package."""
 
-__version__ = "2.0.0-beta.5.2"
+__version__ = "2.0.0-beta.5.3"
 
 from .amazon_parser import AmazonParser
 from .parser_fixes import apply_parser_fixes
